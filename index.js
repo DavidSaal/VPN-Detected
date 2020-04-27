723500cac60c5d56cec9403422778b46bbd3c9d5
David Saal <daviid.saal@gmail.com>
2020-04-27 15:49:18 +0300

    Update

--- index.js
+++ index.js
@@ -131,38 +131,41 @@ function isTor(){
 
 
 function WebRTC(){
-  var clientIP = document.getElementById("clientIP").innerHTML;
-  var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-  var pc = new myPeerConnection({iceServers: [{urls: "stun:stun.l.google.com:19302"}]}),
-    noop = function() {},
-    localIPs = {},
-    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-    key;
-    document.getElementById("A").innerHTML = window.RTCPeerConnection;
-    document.getElementById("B").innerHTML = window.mozRTCPeerConnection;
-    document.getElementById("C").innerHTML = window.webkitRTCPeerConnection;
-    if(!window.webkitRTCPeerConnection) check();
-
-  function ipIterate(ip) {
-    if (!localIPs[ip]);
-    localIPs[ip] = true;
+  try{
+    var clientIP = document.getElementById("clientIP").innerHTML;
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({iceServers: [{urls: "stun:stun.l.google.com:19302"}]}),
+      noop = function() {},
+      localIPs = {},
+      ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+      key;
+      if(!window.webkitRTCPeerConnection) check();
+  
+    function ipIterate(ip) {
+      if (!localIPs[ip]);
+      localIPs[ip] = true;
+    }
+    
+    pc.createDataChannel("");
+    
+    pc.createOffer(function(sdp) {
+      sdp.sdp.split('\n').forEach(function(line) {
+        if (line.indexOf('candidate') < 0) return;
+        line.match(ipRegex).forEach(ipIterate);
+      });
+      pc.setLocalDescription(sdp, noop, noop);
+    }, noop);
+    
+    pc.onicecandidate = function(ice) {
+      if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+      ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+      check()
+    };
   }
-  
-  pc.createDataChannel("");
-  
-  pc.createOffer(function(sdp) {
-    sdp.sdp.split('\n').forEach(function(line) {
-      if (line.indexOf('candidate') < 0) return;
-      line.match(ipRegex).forEach(ipIterate);
-    });
-    pc.setLocalDescription(sdp, noop, noop);
-  }, noop);
-  
-  pc.onicecandidate = function(ice) {
-    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-    ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
-    check()
-  };
+  catch (e) {
+    check();
+    console.log("WebRTC Error:" + e);
+  }
 
   function check(){
     if(!Object.keys(localIPs)[0] && !Object.keys(localIPs)[1]){
